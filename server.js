1d637a3602e7fa2ee287a740f39369f877734da6
pankaj <2pankaj.jain@gmail.com>
2015-11-26 18:16:51 +0530

    add post for todo

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app =express();
 var PORT = process.env.PORT || 3000;
-var todos =[{
+/*var todos =[{
 	id:1,
 	description:'meeting mom',
 	completed:false
@@ -13,7 +14,12 @@ var todos =[{
 	id:3,
 	description:'party time',
 	completed:true
-}];
+}];*/
+
+var todos =[];
+
+var todoNextId =1;
+app.use(bodyParser.json());
 
 app.get('/',function(req,res){
  	res.send('todo api root');
@@ -42,6 +48,18 @@ app.get('/todos/:id',function(req,res){
  	//res.json('Asking for to do with id of'+req.params.id);
 });
 
+//Post
+app.post('/todos',function(req,res){
+	var body =req.body;
+	//add id field
+	body.id = todoNextId++;
+	//todoNextId++;
+	//push body into array
+	todos.push(body);
+	//console.log('description'+body.description);
+	res.json(body);
+
+});
 
 app.listen(PORT,function(){
 	console.log("kdhbfhdsb"+PORT);
